1e380ee578cf4b54db95103b3cff94bda5e606bd
ArtemKVD <a.kunicza@list.ru>
2025-08-01 23:37:43 +0300

    dashboard

---
+++ dashboard/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+
+	http.HandleFunc("/ratings", func(w http.ResponseWriter, r *http.Request) {
+		ratings, err := rdb.ZRevRangeWithScores(context.Background(), "product_ratings", 0, 10).Result()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		for _, item := range ratings {
+			fmt.Fprintf(w, "%s: %v \n", item.Member, item.Score)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
